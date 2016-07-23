b60ed71b536b16d9da9abffbe4a4d235a85830a3
yaniv-l <yanivl@srl.co.il>
2016-07-23 11:15:23 +0300

    Craete simple reducer for books and combinded it to books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export defualt function (){
+  return[
+    { title: 'JavaScript: The Good Parts'},
+    { title: 'Harry Potter'},
+    { title: 'The Dark Tower'},
+    { title: 'Eloquent Ruby'}
+  ]
+}
